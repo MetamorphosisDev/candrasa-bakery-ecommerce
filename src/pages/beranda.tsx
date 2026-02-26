ede934708e1eef3971c3095fe9f386ca8eecedae
Jona Al Farros <124805962+MetamorphosisDev@users.noreply.github.com>
2026-02-27 04:37:33 +0700

    Test

--- src/pages/beranda.tsx
+++ src/pages/beranda.tsx
@@ -2,7 +2,7 @@ function Beranda() {
     return (
         <div className="beranda-page">
             <div className="beranda-page-top beranda-page-top bg-gray-light px-4 sm:px-6 lg:px-8">anjay test</div>         
-            <div className="beranda-page-category-product"></div>
+            <div className="beranda-page-category-product "></div>
         </div>
     )
 }
