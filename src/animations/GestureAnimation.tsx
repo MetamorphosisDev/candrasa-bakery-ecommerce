55e0710c7d72b1992fed1fcd88dbba1246264fed
Jona Al Farros <124805962+MetamorphosisDev@users.noreply.github.com>
2026-04-06 13:47:48 +0700

    Update

--- src/animations/GestureAnimation.tsx
+++ src/animations/GestureAnimation.tsx
@@ -1,27 +1,27 @@
-import * as motion from "motion/react-client"
+import * as motion from 'motion/react-client';
 
 export default function GestureCard({
-    children
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    return (
-        <motion.div
-            whileHover={{
-                scale: 1.05,
-                y: -5
-            }}
-            whileTap={{
-                scale: 0.95
-            }}
-            transition={{
-                type: "spring",
-                stiffness: 300,
-                damping: 20
-            }}
-            className="rounded-xl shadow-lg cursor-pointer"
-        >
-            {children}
-        </motion.div>
-    )
+  return (
+    <motion.div
+      whileHover={{
+        scale: 1.05,
+        y: -5,
+      }}
+      whileTap={{
+        scale: 0.95,
+      }}
+      transition={{
+        type: 'spring',
+        stiffness: 300,
+        damping: 20,
+      }}
+      className="rounded-xl shadow-lg cursor-pointer"
+    >
+      {children}
+    </motion.div>
+  );
 }
